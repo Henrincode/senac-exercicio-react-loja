e1f69c3936cba33b96447c2d65ba6f329e12bf74
HENRIQUE ALAN RODRIGUES MARQUES <henrique.armarques@senacsp.edu.br>
2025-08-04 21:13:08 -0300

    feat: Carrinho.jsx recebendo props

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -1,8 +1,24 @@
-export default function Carrinho() {
+export default function Carrinho({ carrinho }) {
+  const total = carrinho.reduce((soma, item) => soma + item.preco, 0)
+
   return (
     <main>
       <h2>Carrinho de Compras</h2>
-      <p>Ainda não há produtos no carrinho.</p>
+
+      {carrinho.length === 0 ? (
+        <p>Seu carrinho está vazio.</p>
+      ) : (
+        <>
+          <ul>
+            {carrinho.map((item, index) => (
+              <li key={index}>
+                {item.nome} - R$ {item.preco}
+              </li>
+            ))}
+          </ul>
+          <p><strong>Total:</strong> R$ {total}</p>
+        </>
+      )}
     </main>
   )
 }
